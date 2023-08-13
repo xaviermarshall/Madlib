5be4bd33b7b1b94abea5923778a1aaed17bfaf4c
xaviermarshall <53165072+xaviermarshall@users.noreply.github.com>
2023-08-13 12:08:51 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,33 @@
+# madlibs project
+
+# noun variables
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+noun3 = input("Noun: ")
+noun4 = input("Plural Noun: ")
+noun5 = input("Noun: ")
+noun6 = input("Noun: ")
+noun7 = input("Noun: ")
+noun8 = input("Noun: ")
+
+# verb variables
+verb1 = input("Verb: ")
+verb2 = input("Verb ending in -ing: ")
+verb3 = input("Verb: ")
+verb4 = input("Past Tense Verb: ")
+verb5 = input("Past Tense Verb: ")
+
+# adjective variables
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+
+# adverb variable
+adv1 = input("Adverb: ")
+
+# preposition variable
+prep1 = input("Preposition: ")
+
+
+madlib = f"Last night I saw a {noun1} {verb1} a {noun2} with {adj1} ferocity! I never imagined such a(n) {adj2} sight. After that, on my way to the {noun3}, I saw 3 {noun4} eating a {noun5}. They were really {verb2} that thing. When I saw the {noun6} trying to {verb3} my {noun7}, I {verb4} {prep1} it and really {verb5} it! That was a {adv1} {adj3} {noun8}!"
+print(madlib)
